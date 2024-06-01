8b60c4f050bb3e3c20c9cb1f925e7fbd9593872b
MeyDetour <meydetour@gmail.com>
2024-06-01 17:14:29 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+require '../vendor/autoload.php';
+$router = new AltoRouter();
+
+
+$router->map( 'GET', '/', function() {
+    App\View\View::render('home',[]);
+
+});
+// map users details page
+$router->map( 'GET|POST', '/page/[i:id]/', function( $id ) {
+    App\View\View::render('p'.$id,[]);
+
+});
+
+
+// match current request
+$match = $router->match();
+
+// call closure or throw 404 status
+if ($match && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+} else {
+    // no route was matched
+    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
