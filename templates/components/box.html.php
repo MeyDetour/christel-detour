caf8d9f726ce65807b406cb15152cdf3d7209778
Maxence ABRILE <maxenceabrile@icloud.com>
2024-06-02 20:05:05 +0100

    Starting home page

---
+++ templates/components/box.html.php
@@ -0,0 +1,4 @@
+<div class="box" onclick="location.href = '<?= $values[2] ?>'" style="background-color: <?= $values[3]; ?>">
+	<img src="<?= $values[1]; ?>" alt="">
+	<?php echo $values[0]; ?>
+</div>
