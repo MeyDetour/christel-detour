a5eabc5d295ea90d254fbec8fc9754145b3e80f9
mey <meydetour@gmail.com>
2024-09-25 19:27:12 +0200

    responsif - image of theme of activity page

--- templates/themeOfActivite.html.php
+++ templates/themeOfActivite.html.php
@@ -15,25 +15,17 @@
     // Vérifiez si le dossier existe
     if (is_dir($chemin)) {
         $images = glob($chemin . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
-
-        // Parcourez les images trouvées et les distribuer dans les colonnes
-        foreach ($images as $index => $image) {
-            $columns[$index % 3][] = $image;  // Répartition des images dans les colonnes
-        }
-
     } else {
         echo "Le dossier n'existe pas.";
     }
     ?>
+
     <div class=" activityColumns">
-        <?php for ($i = 0; $i < 3; $i++): ?>
-            <div class="flexCol activityColumn">
-                <?php foreach ($columns[$i] as $image): ?>
-                    <img class="activityImage"
-                         src="<?php echo '/pictures/' . $themeOfActivité . '/' . basename($image); ?>"
-                         alt="<?php echo basename($image); ?>">
-                <?php endforeach; ?>
-            </div>
-        <?php endfor; ?>
+        <?php foreach ($images as $image): ?>
+            <img class="activityImage"
+                 src="<?php echo '/pictures/' . $themeOfActivité . '/' . basename($image); ?>"
+                 alt="<?php echo basename($image); ?>">
+        <?php endforeach; ?>
+
     </div>
 </div>
