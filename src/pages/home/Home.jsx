c17baa0d343d200decdd4fc6a762eaccad47b277
mey <meydetour@gmail.com>
2025-01-18 17:00:36 +0100

    feat : home responsiv and change first section html in home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,9 +9,11 @@ export const Home = () => {
 					<path d="M91.3017 75.2794C113.257 58.5615 138.027 45.9079 164.44 37.9174L251.053 11.715C285.292 1.35709 321.343 -1.59284 356.811 3.06141L519.364 24.3929C631.015 39.0445 714.5 134.201 714.5 246.809V246.809C714.5 284.018 705.244 320.643 687.566 353.384L643.5 435L632.032 451.71C569.533 542.78 447.162 569.857 352.076 513.656L263.143 461.091C236.888 445.573 203.591 448.941 180.976 469.402V469.402C150.635 496.854 103.278 492.361 78.6413 459.693L46.0791 416.516C38.0827 405.914 31.9159 394.048 27.8339 381.41L11.3407 330.349C-18.8209 236.972 13.2317 134.727 91.3017 75.2794V75.2794Z" fill="#F9D6FF"/>
 				</svg>
 
-				<svg className="line" viewBox="0 0 1920 627" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M1987.99 330.161C1987.99 330.161 1804.99 427.161 1669.99 385.161C1534.99 343.162 1632.49 46.6612 1418.99 4.66115C1205.49 -37.3389 1019.49 292.661 1100.99 449.661C1182.49 606.662 1348.49 545.161 1404.99 449.661C1461.49 354.162 1448.49 245.661 1269.99 162.661C1091.49 79.6611 805.49 -1.33861 625.489 4.66115C445.487 10.6609 324.491 92.1611 149.989 449.661C-24.5124 807.162 -209.011 507.161 -137.01 527.661" stroke="#0E0E0E"/>
+				<svg className="line" viewBox="0 0 1920 477" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M1988 251.143C1988 251.143 1805 324.857 1670 292.939C1535 261.022 1632.5 35.6998 1419 3.78225C1205.5 -28.1353 1019.49 222.645 1101 341.955C1182.5 461.266 1348.5 414.53 1405 341.955C1461.5 269.381 1448.5 186.928 1270 123.853C1091.5 60.7777 805.494 -0.777201 625.492 3.78225C445.489 8.3417 324.493 70.2769 149.99 341.955C-24.5119 613.634 -209.011 385.652 -137.01 401.231" stroke="#0E0E0E"/>
 				</svg>
+
+
 
 				<div className="flexCol title">
 					<h1>DETOUR Christel</h1>
@@ -20,29 +22,29 @@ export const Home = () => {
 
 				<div className="flexRow homeBoxes">
 					<div className="flexCol">
-						<p className="sm-text">Faites défiller l’écran<br/>pour mieux me connaître.</p>
+						<p className="md-text">Faites défiller l’écran<br/>pour mieux me connaître.</p>
 						<img src="./icons/mouse.svg" alt="Scroll down" />
 						<img src="./icons/small-down-arrow.svg" alt="Scroll down" />
 					</div>
 					<div className="flexRow">
-						<div>
-							<p className="md-text">Apprenez à me connaître</p>
+
+							<h3 >Apprenez à me connaître</h3>
 							<div className="flexCol">
-								<p className="sm-text">Qui-suis je ?</p>
-								<p className="sm-text">Mes formations</p>
+								<p className="md-text">Qui-suis je ?</p>
+								<p className="md-text">Mes formations</p>
 							</div>
-						</div>
+
 					</div>
 					<div className="flexRow">
-						<div>
-							<p className="md-text">Mon activité</p>
+
+							<h3 >Mon activité</h3>
 							<div className="flexCol">
-								<p className="sm-text">Mon rôle</p>
-								<p className="sm-text">Accompagnement</p>
-								<p className="sm-text">Lieu d’accueil</p>
-								<p className="sm-text">Organisation</p>
+								<p className="md-text">Mon rôle</p>
+								<p className="md-text">Accompagnement</p>
+								<p className="md-text">Lieu d’accueil</p>
+								<p className="md-text">Organisation</p>
 							</div>
-						</div>
+
 					</div>
 				</div>
 			</section>
