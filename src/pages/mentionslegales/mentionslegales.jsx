460eba0f60914ec379777a71b8a064d519b1a757
Mey <146076748+MeyDetour@users.noreply.github.com>
2025-06-12 16:39:57 +0200

    Create mentionslegales.jsx

---
+++ src/pages/mentionslegales/mentionslegales.jsx
@@ -0,0 +1,71 @@
+
+import HorizontalSeparator from './../../components/HorizontalSeparator';
+import "./style.css"
+export function Mentionslegales() {
+
+ 
+	return (
+		<>
+	 
+
+				<h2>Mentions Légales :</h2>
+				<div>
+             <HorizontalSeparator text="1. Édition du site"/>
+		
+					<p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet https://christel-detour.com l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:</p>
+<p>Propriétaire du site : Christel Detour - Contact : contact@christel-detour.com 0651484225 - Adresse : 101 Chemin de Ronde.</p>
+
+					<p>Identification de l'entreprise : Christel Detour - Adresse postale : 14 allée des sources Simandres 69360 </p>
+
+					<p>Directeur de la publication :Christel Detour : contact@christel-detour.com.</p>
+
+					<p>Hébergeur : AutreLinode 249 Arch Street, Philadelphia, PA 19106, USA +33-156697240</p>
+
+					<p>Délégué à la protection des données :  Christel Detour - contact@christel-detour.com</p>
+
+					<p>Webmastr : GENOS CENTER by Mey Detour – contact@md-genos.com - 07 56 91 98 17 - www.genos-center.com <br/></p>
+				</div>
+
+				<div>
+				 <HorizontalSeparator text="2. Propriété intellectuelle et contrefaçons."/>
+					<p>Christel Detour est propriétaire des droits de propriété intellectuelle et détient les droits d’usage sur tous les éléments accessibles sur le site internet,
+            notamment les textes, images, graphismes, logos, vidéos, architecture, icônes et sons.</p>
+
+					<p>Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé
+            , est interdite, sauf autorisation écrite préalable de Christel Detour.</p>
+
+					<p>Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie
+            conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
+				</div>
+				<div>
+				 <HorizontalSeparator text="3. Limitations de responsabilité."/>
+					<p>Christel Detour ne pourra être tenu pour responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site https://christel-detour.com.</p>
+
+					<p>Christel Detour décline toute responsabilité quant à l’utilisation qui pourrait être faite des informations et contenus présents sur https://christel-detour.com.</p>
+
+					<p>Christel Detour s’engage à sécuriser au mieux le site https://christel-detour.com, cependant sa responsabilité ne pourra être mise en cause si des données indésirables 
+            sont importées et installées sur son site à son insu.</p>
+
+					<p>Des espaces interactifs (espace contact ou commentaires) sont à la disposition des utilisateurs. Christel Detour se réserve le droit de supprimer, sans mise en demeure 
+            préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données.</p>
+
+					<p>Le cas échéant, Christel Detour se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de
+            message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie …).</p>
+				</div>
+				<div>
+					 <HorizontalSeparator text="4. CNIL et gestion des données personnelles."/>
+					<p>Conformément aux dispositions de la loi 78-17 du 6 janvier 1978 modifiée, l’utilisateur du site https://christel-detour.com dispose d’un droit d’accès, 
+            de modification et de suppression des informations collectées. Pour exercer ce droit, envoyez un message à notre Délégué à la Protection des Données : Chrisltel Detour - contact@christel-detour.com.</p>
+
+		 		</div>
+				<div>
+					 <HorizontalSeparator text="6. Droit applicable et attribution de juridiction."/>
+					<p>Tout litige en relation avec l’utilisation du site https://christel-detour.com est soumis au droit français. En dehors des cas où la loi ne le permet pas, il est fait attribution exclusive de juridiction aux tribunaux compétents de Lyon.</p>
+				</div>
+
+
+			</div>
+		</>
+
+	);
+}
